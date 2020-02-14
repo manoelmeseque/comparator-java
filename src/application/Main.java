ccfa204c8a44a98a322882a1a67be115d5889e3a
manoelmeseque <meseque.teixeira@outlook.com>
2020-02-14 14:26:45 -0300

    Lambda 3

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,9 +15,7 @@ public class Main {
 		list.add(new Product("Notebook", 1200.0));
 		list.add(new Product("Tablet", 450.0));
 		
-		Comparator<Product> comp = (p1, p2) -> {
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		};
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		
 		list.sort(comp);
 		
